5c9dc65836cab47f0f1155ab80e030bf4c70a298
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2019-08-11 21:32:52 +0700

    Update board_control.jquery.js

--- board_control.jquery.js
+++ board_control.jquery.js
@@ -1,5 +1,5 @@
 $(function(){
-    var ip = $("#device_ip").text();
+    var ip = $("#device_ip").val();
     var device = new Device(ip);
     $("#send").click(function(){
         var text = $("#sending_text").text();
