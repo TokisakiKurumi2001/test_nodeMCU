b44ffbed35abed63d596705882ae66467cedb51f
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2019-08-13 10:56:47 +0700

    Update board_control

--- board_control.jquery.js
+++ board_control.jquery.js
@@ -2,7 +2,6 @@ $(function(){
     var ip = $("#device_ip").text();
     var device = new Device(ip);
     $("#send").click(function(){
-        var text = $("#sending_text").text();
-        device.callFunction('led',text);
+        
     });
 })
