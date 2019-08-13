9cc7ea2fbc6fad0ee40bc7ebbf56d84a55a2079d
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2019-08-13 11:32:06 +0700

    Update final time for linking

--- testing.jquery.js
+++ testing.jquery.js
@@ -1,12 +1,7 @@
-$(function(){
-    function updateLink(message)
-    {
-        var new_link = "http://";
-        var addr = $("#device_ip").val();
-        new_link += addr;
-        new_link += "/led?params=";
-        var send_message = message;
-        new_link += send_message;
-        $("#id").attr("href",new_link);
-    }
-})
+function updateLink(message)
+{
+    var new_link = "http://";
+    var addr = document.getElementById("device_ip").value;
+    new_link += addr + "/led?params=" + message;
+    document.getElementById("link").href = new_link;
+}
