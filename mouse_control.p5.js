6b0787fb624db18b23d2a4db007a7058436a7929
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2019-08-25 20:42:37 +0700

    Adding mouse control feature, first test, the responsive design

---
+++ mouse_control.p5.js
@@ -0,0 +1,12 @@
+function setup()
+{
+    var size = document.querySelector("#p5_mouse_control").offsetWidth;
+    var canvas = createCanvas(size, size).parent("p5_mouse_control");
+    colorMode(HSL);
+    background(236,31,20);
+}
+
+function draw()
+{
+
+}
