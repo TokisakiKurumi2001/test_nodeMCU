e4da8e5720b4a583c06533c2990e7cb047e00311
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2019-10-04 08:45:51 +0700

    Update the IP address of the device

--- update_link.js
+++ update_link.js
@@ -2,6 +2,6 @@ function updateLink(message)
 {
     var new_link = "http://";
     var addr = document.getElementById("device_ip").value;
-    new_link += addr + "/led?params=" + message;
+    new_link += addr + "/keyboard?params=" + message;
     document.getElementById("link").href = new_link;
 }
