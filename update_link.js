733fe0214520501a11110833b2fe5b46f6c4f963
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2019-10-27 21:36:08 +0700

    Explain code written in update_link.js

--- update_link.js
+++ update_link.js
@@ -1,5 +1,17 @@
+/*
+*   This function is called when `format` inside the vue `app` changed
+*   This function receive a string then replace the old link to control 
+*   keyboard with new message to control keyboard
+*
+*   @params message: message receive(aka formatted string)
+*/
 function updateLink(message)
 {
+    /*
+    *   Get the value of `device_ip` input and concat with some parameters
+    *   and the `message`, then replace the link of the button establishing
+    *   controlling to the keyboard
+    */
     var new_link = "http://";
     var addr = document.getElementById("device_ip").value;
     new_link += addr + "/keyboard?params=" + message;
