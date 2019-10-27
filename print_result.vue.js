04bc3a4ee707f6c6f7466056ec1af150dd18d6fc
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2019-10-27 16:56:27 +0700

    Explain code written print_result.vue.js

--- print_result.vue.js
+++ print_result.vue.js
@@ -6,10 +6,23 @@ var app = new Vue({
     }
 })
 
-app.$watch("message", function(newValue, oldValue) {
+/*
+*   when there is any change in the value `message`
+*   which means that the user record voice command or type in the command.
+*   We will remove some Vietnamese accent of the newValue of `message`
+*   (aka: new command from user)
+*/
+app.$watch("message", function(newValue, _) {
     format_symbol(newValue);
 })
 
-app.$watch("format", function(newValue, oldValue) {
+/*
+*   When the `format` value change, which mean that the accent has been removed
+*   We will update the link for the keyboard command button.
+*   There is a button which has the link to open in new page and address to
+*   establish command on nodeMCU, we need to update the link of that button
+*   so that is has the right command to deliver
+*/
+app.$watch("format", function(newValue, _) {
     updateLink(newValue);
 })
