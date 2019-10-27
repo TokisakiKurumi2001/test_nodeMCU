7e72fb2d43cfcc8396c11f52c92d7f627d672495
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2019-10-27 21:40:50 +0700

    Explain code written in symbol_format.js

--- symbol_format.js
+++ symbol_format.js
@@ -1,13 +1,25 @@
-function format_symbol(empty_str)
+/*
+*   This function is called when `message` in vue `app` change value
+*   This function receives a string then pass the new formatted value
+*   out the webpage through Vue pipeline `console.log(app.format=res)`
+*
+*   @param my_str: message to remove Vietnamese accent
+*/
+function format_symbol(my_str)
 {
-	var res = nonAccentVietnamese(empty_str);
+	var res = nonAccentVietnamese(my_str);
     console.log(app.format = res);
 }
 
+/*
+*   This function receive a string, then replace all the Vietnamese accent with
+*   alphabetic letter
+*
+*   source: https://gist.github.com/jarvisluong/f01e108e963092336f04c4b7dd6f7e45#file-nonaccentvietnamese-js
+*/
 function nonAccentVietnamese(str)
 {
     str = str.toLowerCase();
-    // We can also use this instead of from line 11 to line 17
     str = str.replace(/\u00E0|\u00E1|\u1EA1|\u1EA3|\u00E3|\u00E2|\u1EA7|\u1EA5|\u1EAD|\u1EA9|\u1EAB|\u0103|\u1EB1|\u1EAF|\u1EB7|\u1EB3|\u1EB5/g, "a");
     str = str.replace(/\u00E8|\u00E9|\u1EB9|\u1EBB|\u1EBD|\u00EA|\u1EC1|\u1EBF|\u1EC7|\u1EC3|\u1EC5/g, "e");
     str = str.replace(/\u00EC|\u00ED|\u1ECB|\u1EC9|\u0129/g, "i");
